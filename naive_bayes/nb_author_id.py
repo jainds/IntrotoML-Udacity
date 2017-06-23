fc082689c7f5a6c2167e169ad5d8f7ba6c988365
Piyushkumar S. Jain <dsjain.1793@gmail.com>
2017-06-23 14:11:04 +0530

    NaiveBayes mini project completed
    We used sklearn naive bayes module to predict whether the mails were
    from Sara or Chris.
    
    we achived an accuracy score of 97.3265%
    
    no. of Chris training emails: 7936
    no. of Sara training emails: 7884
    training time: 2.16 s
    prediction time: 0.316 s
    0.973265073948

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -31,3 +31,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 
 
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+ans = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+print accuracy_score(ans, labels_test)
