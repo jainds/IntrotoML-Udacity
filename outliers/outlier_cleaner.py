e77366be4df12b80381ae1960cc0d1a24a13f7ca
Piyushkumar S. Jain <dsjain.1793@gmail.com>
2017-06-28 14:20:26 +0530

    Kmeans Clustering Algorithm Practice

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -12,9 +12,22 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
-
+	
+	
     ### your code goes here
 
+    work_tup = []
     
-    return cleaned_data
+    for index in range(len(ages)):
+     aa = ages[index]
+     ee = predictions[index] - net_worths[index] 
+     nn = net_worths[index]
+     pp = predictions[index]
+     t = [aa[0], nn[0], ee[0]]
+     work_tup.append(t)
+        
+    work_tup = sorted(work_tup, key=lambda x: x[2])
+    
+    return work_tup[:81]
+    
 
