e2ebb7de588aca474a6e6a2053480557869a6721
Piyushkumar S. Jain <dsjain.1793@gmail.com>
2017-06-29 11:28:29 +0530

    Text learning
    Performed stemming, removing sugnatures, removing stop words and
    applying transformation using tf-idf

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -26,18 +26,21 @@ def parseOutText(f):
     if len(content) > 1:
         ### remove punctuation
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
-
+        word = text_string.split()
+        print word
+        sme = SnowballStemmer("english")
+        stemwo =[]
+        for each in word:		
+			stemwo.append(sme.stem(each))
+        print stemwo
+        words = ' '.join(stemwo)
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
-
+				
     return words
 
     
